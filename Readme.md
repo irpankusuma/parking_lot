17e02cbb12f78a3b7ff017aa697a9a73e9a1e391
unknown <anonymous.ikwijaya@gmail.com>
2020-02-18 22:37:38 +0700

    update result and images

--- Readme.md
+++ Readme.md
@@ -1,1 +1,39 @@
-'# parking_lot dk' 
+# parking_lot dk
+Author : irpan.kusuma@gmail.com / anonymous.ikwijaya@gmail.com
+
+- Requirement
+    - Node JS
+
+# How to Use
+- Open index.js, then change
+- Scroll to bottom, at Section @Example
+- Added your string text, example
+    - only accepted four function
+    - create_parking_lot {n}
+    - park {car_number}
+    - leave {car_number} // calculate datetime
+    - status    
+- Open command line, and goto this project parking_lot
+- parking_lot #>node index.js
+
+# Example
+<code>
+    let str = 'create_parking_lot 6 \n' 
+    str += 'park KA-01-HH-1234 \n'             
+    str += 'park KA-01-HH-9999 \n'             
+    str += 'park KA-01-BB-0001 \n'             
+    str += 'park KA-01-HH-7777 \n'             
+    str += 'park KA-01-HH-2701 \n'             
+    str += 'park KA-01-HH-3141 \n'    
+    str += 'leave KA-01-HH-3141 \n'             
+    str += 'status \n'    
+    str += 'park KA-01-P-333 \n'    
+    str += 'park DL-12-AA-9999 \n'    
+    str += 'leave KA-01-HH-1234 \n'    
+    str += 'leave KA-01-BB-0001 \n'
+    str += 'leave DL-12-AA-9999 \n'
+    str += 'park KA-09-HH-0987 \n'
+    str += 'park CA-09-IO-1111 \n'
+    str += 'park KA-09-HH-0123 \n'
+    str += 'status'; 
+</code>
--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 'use strict';
 
+/**
+ * Author : irpan.kusuma@gmail.com
+ * Class ParkingLot
+ * data_parking = for store every user create new slot, update (added) slot, or delete (leave) slot
+ * car_no = for handling input because as global
+ */
 class ParkingLot {
     constructor(){
         this.data_parking = [];
         this.car_no = null;
     }
 
+    /**
+     * func insertCarNo
+     * Handle global car_no
+     */
     set insertCarNo(car_no){
         this.car_no = car_no;
     }
 
+    /**
+     * func insert
+     * Handle for added data within global
+     */
     set insert(n=0){
         let _data = [];
         try {
@@ -29,6 +43,11 @@ class ParkingLot {
         }
     }
 
+    /**
+     * func update
+     * Handle change data by id
+     * Example data = { id: 1, data: { obj_data } }
+     */
     set update(data={}){
         try {
             let filter = []
@@ -40,6 +59,10 @@ class ParkingLot {
         }
     }
 
+    /**
+     * func selectByCarNo
+     * Handle data when car_no equal car_no
+     */
     get selectByCarNo(){
         try {
             let filter = this.data_parking.filter((k) => k.car_no == this.car_no);
@@ -50,6 +73,10 @@ class ParkingLot {
         }
     }
 
+    /**
+     * func selectFreeSlot
+     * Handle data when car_no is empty, or free slot
+     */
     get selectFreeSlot(){
         try {
             return this.data_parking.filter((k) => k.car_no == null);
@@ -58,6 +85,10 @@ class ParkingLot {
         }
     }
 
+    /**
+     * funct select
+     * Handle all data for show status
+     */
     get select(){
         try {
             return this.data_parking;    
@@ -66,7 +97,12 @@ class ParkingLot {
         }
     }
 
-    /// Function
+    /**
+     * Function for first time handle input
+     * String must be have <\n> enter, for split to array
+     * @param {*} string 
+     * @result as string
+     */
     splitFn(string=null){
         let split = [];
         let output = [];
@@ -92,11 +128,23 @@ class ParkingLot {
         return output.join("\n");
     }
 
+    /**
+     * For create parking lot
+     * @param {*} n must be number
+     * @result as string
+     */
     createParkingLot(n=0){
+        if(Number.isInteger(n))
+            return 'Please input integer';
+
         this.insert = n;
         return 'Created parking lot with '+n+ ' slot(s).';
     }
 
+    /**
+     * For parking lot, update global data_parking
+     * @param {*} car_no 
+     */
     park(car_no=null){
         let freeSlot = this.selectFreeSlot;
         if(freeSlot.length == 0){
@@ -121,6 +169,10 @@ class ParkingLot {
         return 'Allocated slot number : ' + Math.min(...n);
     }
 
+    /**
+     * For update global data parking
+     * @param {*} car_no 
+     */
     leave(car_no=null){
         this.insertCarNo = car_no;
         let filter = this.selectByCarNo;
@@ -144,9 +196,24 @@ class ParkingLot {
 
         /// calculate pembayaran 
         let calculate = this.billCalculate(start);
-        return 'Registration number '+car_no+' with Slot Number '+id+' is free with Charge '+calculate;
+        let totalFree = this.selectFreeSlot.length;
+
+        let output = '\n======== BILLING PAYMENT ========== \n';
+        output += 'Registration No  : '+car_no+' \n';
+        output += 'Last Slot No     : '+id+' \n';
+        output += 'Free Slot No     : '+id+' \n';
+        output += 'Total Free       : '+totalFree+' slot(s) \n';
+        output += 'Start Time       : '+calculate.dateStart+' \n';
+        output += 'End Time         : '+calculate.dateEnd+' \n';
+        output += 'Charge           : '+calculate.result+' for '+calculate.hours+' hour(s) \n';
+        output += '===================================';
+        return output;
     }
 
+    /**
+     * For calculate price
+     * @param {*} start 
+     */
     billCalculate(start=null){
         let dateEnd = new Date().getHours();
         let dateStart = new Date(start).getHours();
@@ -161,12 +228,21 @@ class ParkingLot {
             }
         }
 
-        return finish.length > 0 ? finish.reduce((a,b) => a+b) : 0
+        return {
+            result: finish.length > 0 ? finish.reduce((a,b) => a+b) : 0,
+            dateStart: new Date(start),
+            dateEnd: new Date(),
+            hours: calculate
+        }
     }
 
+    /**
+     * For show status parking
+     */
     status(){
         let data = this.select;
-        let output = 'Slot No.  Registration No. \n';
+        let output = '\n======== STATUS PARKING LOT ========== \n';
+        output += 'Slot No.  Registration No. \n';
         data.sort((a,b) => a.id - b.id);
         
         for(let i=0; i<data.length; i++){ 
@@ -176,21 +252,42 @@ class ParkingLot {
             output += data[i].car_no == null ? 'Free \n' : data[i].car_no+ ' \n'
         }
 
+        output += '======================================';
         return output;
     }
 }
 
+/**
+ * Section @Example
+ */
+
+// change here ...
+let str = 'create_parking_lot 6 \n' 
+str += 'park KA-01-HH-1234 \n'             
+str += 'park KA-01-HH-9999 \n'             
+str += 'park KA-01-BB-0001 \n'             
+str += 'park KA-01-HH-7777 \n'             
+str += 'park KA-01-HH-2701 \n'             
+str += 'park KA-01-HH-3141 \n'    
+str += 'leave KA-01-HH-3141 \n'             
+str += 'status \n'    
+str += 'park KA-01-P-333 \n'    
+str += 'park DL-12-AA-9999 \n'    
+str += 'leave KA-01-HH-1234 \n'    
+str += 'leave KA-01-BB-0001 \n'
+str += 'leave DL-12-AA-9999 \n'
+str += 'park KA-09-HH-0987 \n'
+str += 'park CA-09-IO-1111 \n'
+str += 'park KA-09-HH-0123 \n'
+str += 'status'                        
+// end
+
+// don`t change 
 let parkingLot = new ParkingLot();
-let str = 'create_parking_lot 1 \n' // create 3 slot
-str += 'park AB-111 \n'     // alocated
-str += 'park AB-112 \n'     // alocated
-str += 'park AB-113 \n'     // alocated
-str += 'park AB-114 \n'     // full
-str += 'park AB-115 \n'    // leave with calculate
-str += 'leave AB-112 \n'    // leave with calculate
-str += 'status \n'          // status parking
-
-
-// let arr = str.split('\n'); arr.pop();
-// console.log(arr)
 console.info(parkingLot.splitFn(str));
+// end
+
+/**
+ * Result
+ * See folder images
+ */
